ac568b8706255fc34dc475ac4f9817b863a39b70
songqi <289330245@qq.com>
2018-02-06 22:23:30 +0800

    增加第三方授权接口

---
+++ src/main/java/com/alipay/demo/trade/service/AlipayAuthService.java
@@ -0,0 +1,13 @@
+package com.alipay.demo.trade.service;
+
+import com.alipay.api.response.AlipayOpenAuthTokenAppResponse;
+
+/**
+ * Description:<p> </p>
+ * Created by songqi on 2018/2/6.
+ */
+public interface AlipayAuthService {
+
+
+    AlipayOpenAuthTokenAppResponse opentAuthTokenApp();
+}
